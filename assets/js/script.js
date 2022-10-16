330fdf7e2cea3640a90c74ffdd4a17e8169a4e4b
Mike Johnson <mike@mapleprintservices.com>
2022-10-16 18:35:13 -0400

    hour comparison working and set localStorage sending appt data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 var currentDay = document.getElementById('currentDay')
 var now = dayjs().format('dddd, MMMM DD')
+var currentHour = dayjs().hour()
 currentDay.textContent = now
 // var row = $('<p></p>').text('Hello');
 // $('.container').append((row));
@@ -8,43 +9,69 @@ console.log(dayjs());
 // var icon = $('<i class="fa-solid fa-floppy-disk"></i>');
 // $(row).append(icon);
 container = $('.container')
-// sampleHours = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm', '5pm'];
 sampleHours=[]
 sampleHours.length = 9
 $.each(sampleHours, function(index){
-  row = $('<div class="row"></div>');
-  apptTime = dayjs().set('hour', index + 9);
+  row = $('<form class="row" id='+(index + 9)+'></form>');
+  apptTime = dayjs().set('hour', index + 12); //change to 9 after dev
   hour = $('<div class="hour col col-2 col-lg-1">' + apptTime.format('ha') + '</div>');
-  
-  entryField = $('<textarea class="col description"></textarea>');
-  saveBtn = $('<div class="saveBtn col col-2 col-lg-1"></div>')
+  entryField = $('<textarea class="col description" id=entryField-' + index + '></textarea>');
+  saveBtn = $('<button type="submit" class="saveBtn col col-2 col-lg-1"></button>')
   icon = $('<i></i>');
   $(icon).addClass('fa-solid fa-floppy-disk')
-  // row.attr('id', [i]);
   saveBtn.append(icon);
   row.append(hour);
   row.append(entryField);
   row.append(saveBtn);
   container.append(row);
-  $.each(row, function(apptTime) {
-    if (dayjs().isBefore(apptTime) ){
+  $.each(row, function() {
+    apptHour = apptTime.format('H')
+    console.log(apptHour, currentHour)
+    if (currentHour < apptHour){
       row.addClass('future')
-    } else if (dayjs().isSame(apptTime) ){
+    } else if (currentHour == apptHour){
       row.addClass('present')
     } else {
       row.addClass('past')
     }
-  })
-  $.each(row, function(){
-   $(saveBtn).on('click', function(event){
-      event.preventDefault();
-      description = $('.description').val();
-      // console.log('hello')
-      console.log(description);
-    })
+    // if (dayjs().isBefore(apptTime) ){
+    //   row.addClass('future')
+    // } else if (dayjs().isSame(apptTime) ){
+    //   row.addClass('present')
+    // } else {
+    //   row.addClass('past')
+    // }
   })
 })
+// https://stackoverflow.com/questions/45563841/get-val-of-element-multiple-forms-at-one-page-with-jquery
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  var schedule =[];
+  var appointment = $(this).find('textarea').val();
+  var time = $(this).find('.hour').text();
+  var entry = {time, appointment}
+  if (appointment !==''){
+    setSchedule(entry)
+  } else {
+    alert('Please enter an event')
+  }
+}) 
+// get schedule from localStorage
+function getSchedule() {
+  var schedule = localStorage.getItem('schedule');
+  console.log(schedule);
+  return schedule;
+}
+// get and set schedule into localStorage
+function setSchedule(entry) {
+  getSchedule();
+  if (getSchedule() === null){
+      var schedule =[];
+    } else {
+      var schedule = JSON.parse(localStorage.getItem('schedule'));
+    }
+  schedule.push({entry})
+  localStorage.setItem('schedule', JSON.stringify(schedule))
+}
 
-
-console.log($('.row'));
-
+// setSchedule()
